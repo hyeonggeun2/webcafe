558af2a857fb53f38ed2bbd01ab17dd70c79161e
hyeonggeun2 <dhgudrms12@gmail.com>
2019-09-12 01:48:13 +0900

    20190911 개인과제

--- js/webcafe.js
+++ js/webcafe.js
@@ -1,8 +1,18 @@
 var section = $('.board section');
 var tab = $('.tab');
 
+var ul = $('.menu li ul');
+var subMenu = $('.btn-menu');
+
+
 tab.on('click', function(e){
   e.preventDefault();
   section.removeClass('tab-act');
   $(this).parent().parent().addClass('tab-act');
+})
+
+subMenu.on('mouseover focusin', function(e){
+  e.preventDefault();
+  ul.parent().removeClass('menu-act');
+  $(this).parent().addClass('menu-act');
 })
